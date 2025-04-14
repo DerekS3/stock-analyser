6be2f04735496c735546ee6f101ce49da80fa928
DS0eng <derekstrickland3@gmail.com>
2025-04-14 13:58:38 +0100

    Added properties to model and updated title

--- backend/stock_analyser/models.py
+++ backend/stock_analyser/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Stock(models.Model):
+  symbol = models.CharField(max_length=10)
+  name = models.CharField(max_length=100)
+  sector = models.CharField(max_length=100, blank=True)
